e4e2840149858aa2541d68e614fdf2c6f738ccf1
DavideGila <davide.gila@gmail.com>
2023-10-11 15:32:30 +0200

    added script to make appear numbers in the boxes

--- js/script.js
+++ js/script.js
@@ -2,16 +2,28 @@
 // > ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
 // > Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
+const wrapper = document.getElementById('numberWrapper');
+
 for(let i = 1; i <= 100; i++){
+
+    let box = document.createElement('div');
+    wrapper.append(box);
+    wrapper.classList.add('d-flex', 'flex-wrap');
+    box.classList.add('border', 'box')
+    box.innerHTML = i
+
     if(i % 3 === 0 && i % 5 === 0){ 
         const fizzBuzz = 'FizzBuzz';
         console.log(fizzBuzz);
+        box.innerHTML = fizzBuzz;
     } else if(i % 3 === 0){ 
         const fizz = 'Fizz';
         console.log(fizz);
+        box.innerHTML = fizz;
     } else if(i % 5 === 0){ 
         const buzz = 'Buzz';
         console.log(buzz);
+        box.innerHTML = buzz;
     } else {
         console.log(i);
     }
