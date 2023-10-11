10b5a204dff82d8e1feffd3eac068b39b5d7a60e
DavideGila <davide.gila@gmail.com>
2023-10-11 14:56:33 +0200

    created the full cycle for

--- js/script.js
+++ js/script.js
@@ -3,9 +3,16 @@
 // > Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 for(let i = 1; i <= 100; i++){
-    console.log(i)
     if(i % 3 === 0 && i % 5 === 0){ 
         const fizzBuzz = 'FizzBuzz';
         console.log(fizzBuzz);
+    } else if(i % 3 === 0){ 
+        const fizz = 'Fizz';
+        console.log(fizz);
+    } else if(i % 5 === 0){ 
+        const buzz = 'Buzz';
+        console.log(buzz);
+    } else {
+        console.log(i);
     }
 } 
