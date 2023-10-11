6a4b2da2c9a2d17acc0f4fb42e51c8625cb1059c
DavideGila <davide.gila@gmail.com>
2023-10-11 15:53:02 +0200

    added bg to all boxes

--- js/script.js
+++ js/script.js
@@ -9,21 +9,24 @@ for(let i = 1; i <= 100; i++){
     let box = document.createElement('div');
     wrapper.append(box);
     wrapper.classList.add('d-flex', 'flex-wrap');
-    box.classList.add('border', 'box', 'd-flex', 'justify-content-center', 'align-items-center', 'm-1')
+    box.classList.add('border', 'box', 'd-flex', 'justify-content-center', 'align-items-center', 'm-1', 'bg-success')
     box.innerHTML = i
 
     if(i % 3 === 0 && i % 5 === 0){ 
         const fizzBuzz = 'FizzBuzz';
         console.log(fizzBuzz);
         box.innerHTML = fizzBuzz;
+        box.classList.add('bg-danger')
     } else if(i % 3 === 0){ 
         const fizz = 'Fizz';
         console.log(fizz);
         box.innerHTML = fizz;
+        box.classList.add('bg-warning')
     } else if(i % 5 === 0){ 
         const buzz = 'Buzz';
         console.log(buzz);
         box.innerHTML = buzz;
+        box.classList.add('bg-info')
     } else {
         console.log(i);
     }
