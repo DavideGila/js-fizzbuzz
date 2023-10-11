271c7da935c31e7ea5282d859eb2e9ef96d7df4e
DavideGila <davide.gila@gmail.com>
2023-10-11 14:30:56 +0200

    created and if for FizzBuzz

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@
 // > ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
 // > Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
-for(let i= 1; i <= 100; i++){
+for(let i = 1; i <= 100; i++){
     console.log(i)
-}
+    if(i % 3 === 0 && i % 5 === 0){ 
+        const fizzBuzz = 'FizzBuzz';
+        console.log(fizzBuzz);
+    }
+} 
